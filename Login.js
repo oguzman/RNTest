251f87c9cf15a589d4f9bffd6e8ea4ac1e57776a
Paco Chacon <pacochacon94@gmail.com>
2017-08-14 13:27:51 -0500

    Re estructured login and added activity indicator

--- Login.js
+++ Login.js
@@ -1,54 +1,66 @@
 import React, { Component } from 'react'
 import Dimensions from 'Dimensions'
 import createReactClass from 'create-react-class' 
-import { 
+import {
   StyleSheet,
   Text,
   View,
   TextInput,
   Image,
-  TouchableHighlight
-  } from 'react-native'
+  TouchableHighlight,
+  ActivityIndicator,
+  ActivityIndicatorIOS,
+} from 'react-native'
 
-var { height, width } = Dimensions.get('window')
+import Styles from './styles.js'
 
 
 class Login extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      showProgress: false
+    }
   }
   render() {
     return(
-      <View style={styles.view}>
-        <View style= {styles.container}>
+      <View style={Styles.generalView}>
+        <View style= {Styles.generalContainer}>
           <Image
-            style={ styles.imageView }
+            style={ Styles.logoLogin }
             resizeMode={"contain"}
             source={require('./react.png')}
           />
-          <Text style= { styles.headerText }>
+          <Text style= { Styles.loginHeaderText }>
             React Native Test
           </Text>
           <TextInput
             onChangeText = { (text) => this.setState({ username: text }) }
-            style={ styles.inputs }
+            style={ Styles.blueInputs }
             placeholder={'Email'}
             placeholderTextColor={"rgba(198,198,204,1)"}
           />
           <TextInput
             onChangeText = { (text) => this.setState({ password: text })}
-            style={ styles.inputs }
+            style={ Styles.blueInputs }
             placeholder={'Password'}
             placeholderTextColor={"rgba(198,198,204,1)"}
             secureTextEntry = { true }
           />
           <TouchableHighlight
             onPress = {this.onLoginPressed.bind(this)}
-            style = { styles.button }
+            style = { Styles.blueButton }
             activeOpacity={75 / 100}
             underlayColor={"rgb(210,210,210)"}>
-            <Text>Login</Text>
+            <Text style= { Styles.whiteText } >
+              Login
+            </Text>
           </TouchableHighlight>
+          <ActivityIndicator
+            style={ Styles.loader }
+            animating={true}
+            size={'large'}
+          />
         </View>
       </View>
     );
@@ -57,48 +69,10 @@ class Login extends Component {
   onLoginPressed() {
     console.log('username: ' + this.state.username)
     console.log('password: '+ this.state.password)
+    this.setState ({
+      showProgress: true
+    });
   }
 }
-
-const styles = StyleSheet.create({
-  view: {
-    backgroundColor: '#F5FCFF',
-    flex: 1,
-  },
-  container: {
-    alignItems: 'center',
-    padding: 30
-  },
-  logo: {
-    height: 500,
-    width: 500
-  },
-  headerText: {
-    fontSize: 30,
-    marginBottom: 40
-  }, 
-  inputs: {
-    height: 35, 
-    width: width * 0.9,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: "rgba(86,213,250,1)",
-    paddingLeft: 10,
-    marginTop: 10
-  },
-  imageView: {
-    height: 150,
-    width: 150
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: "rgba(86,213,250, 1)",
-    borderRadius: 5,
-    height: 44,
-    justifyContent: 'center',
-    marginTop: 30,
-    width: width * 0.9
-  }
-});
 
 module.exports = Login;
