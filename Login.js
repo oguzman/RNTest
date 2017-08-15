2b651a33dc246ce0b907ae8df90333bb84b52d01
Paco Chacon <pacochacon94@gmail.com>
2017-08-15 13:45:52 -0500

    Added error handling

--- Login.js
+++ Login.js
@@ -82,10 +82,24 @@ class Login extends Component {
       }
     })
     .then((response) => {
+      if(response.status >= 200 && response.status < 300){
+        return response;
+      }
+      throw {
+        badCredentials: response.status == 401,
+        unknownError: response.status != 401
+      }
+    })
+    .then((response) => {
       return response.json();
     })
     .then((results) => {
       console.log(results);
+    })
+    .catch((err) => {
+      this.setState(err);
+    })
+    .finally(() => {
       this.setState({ showProgress: false })
     })
   }
