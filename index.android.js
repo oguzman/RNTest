fe3606d54c5624fe0290b33d7817c64c5f755fe9
Paco Chacon <pacochacon94@gmail.com>
2017-08-18 12:31:25 -0500

    Andoid index refactor

--- index.android.js
+++ index.android.js
@@ -5,16 +5,41 @@
  */
 
 import React, { Component } from 'react';
+import CreateClass from 'create-react-class';
+import {
+  View,
+  Text
+} from 'react-native';
 import {
   AppRegistry,
 } from 'react-native';
+import Login from './Login.js';
+import styles from './styles'
 
-export default class RNTest extends Component {
-  render() {
-    return (
-     <Login />
-    );
+var RNTest = CreateClass ({
+  render: function() {
+    if(this.state.isLoggedIn) {
+      return(
+        <View style = { styles.generalView }>
+          <Text style = { styles.loginHeaderText }>
+            You are logged!
+          </Text>
+        </View>
+      );
+    } else {
+      return (
+        <Login onLogin = { this.onLogin }/>
+      );
+    }
+  },
+  onLogin() {
+    this.setState({ isLoggedIn: true });
+  },
+  getInitialState() {
+    return {
+      isLoggedIn: false
+    }
   }
-}
+});
 
 AppRegistry.registerComponent('RNTest', () => RNTest);
