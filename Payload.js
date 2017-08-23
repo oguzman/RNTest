ed104e3ea300e633a1a1d1228386a74136d535e0
Paco Chacon <pacochacon94@gmail.com>
2017-08-23 15:41:00 -0500

    Added Payload base

---
+++ Payload.js
@@ -0,0 +1,33 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+	ListView,
+	Text,
+	Image,
+	View
+} from 'react-native';
+import styles from './styles';
+import AuthenticationManager from './AuthenticationManager';
+import Moment from 'moment';
+const urlBase = 'https://api.github.com/users/';
+
+class Payload extends Component {
+	constructor(props) {
+		super(props);
+		var ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		})
+		this.state = {
+			dataSource: ds
+		}
+	}
+
+	render(){
+		return(
+			<View>
+			</View>
+		);
+	}
+}
+
+module.exports = Feed;
