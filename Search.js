1c623b36fc0e7f12484ecda97bb53867bd13c6df
Paco Chacon <pacochacon94@gmail.com>
2017-08-25 13:13:04 -0500

    Added Search base view

---
+++ Search.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+	View,
+	TextInput,
+	Text,
+	TouchableHighlight,
+	ActivityIndicator
+} from 'react-native';
+import styles from './styles'
+
+class Search extends Component {
+	constructor(props){
+		super(props);
+		this.state = ({
+			isLoading: false
+		})
+	}
+
+	render() {
+		return(
+			<View style = { styles.generalView } >
+				<Text>
+					Search
+				</Text>
+				<TextInput
+					placeholder = 'Search...'
+					style = { styles.blueInputs }
+				/>
+			</View>
+		);
+	}
+}
+
+module.exports = Search;
